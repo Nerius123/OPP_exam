8e85b2d843f14ac6cd336ef4bcee48181eb26e78
Nerius <nerijus.ponomariovas@gmail.com>
2025-05-27 13:18:00 +0300

    Pridejau my_library.h

---
+++ my_library.h
@@ -0,0 +1,58 @@
+#ifndef MY_LIBRARY_H // Patikrina, ar MY_LIBRARY_H dar nėra apibrėžtas
+#define MY_LIBRARY_H // Jei nėra apibrėžtas, apibrėžia MY_LIBRARY_H (kad kitą kartą būtų atpažintas)
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <numeric>  
+#include <algorithm> 
+#include <fstream>
+#include <sstream>
+#include <chrono>
+#include <stdexcept>
+#include <random>
+#include <utility>
+#include <list>
+#include <deque>
+#include <cassert>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cin;
+using std::ifstream;
+using std::ofstream;
+using std::setw;
+using std::fixed;
+using std::setprecision;
+using std::left;
+using std::sort;
+using std::accumulate;
+using std::istringstream;
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
+using std::to_string;
+using std::pair;
+using std::list;
+using std::deque;
+using std::find;
+using std::find_if;
+using std::search;
+using std::copy;
+using std::remove;
+using std::remove_if;
+using std::remove_copy;
+using std::remove_copy_if;
+using std::transform;
+using std::partition;
+using std::stable_partition;
+using std::move;
+using std::swap;
+using std::exchange;
+using std::stringstream;
+
+
+#endif  // Baigiame header guard (apsauga nuo pakartotinio įtraukimo)
