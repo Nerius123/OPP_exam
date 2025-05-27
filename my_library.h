85d8cd3f6dc82ef8ee6265006b5f9a2a49b643f2
Nerius <nerijus.ponomariovas@gmail.com>
2025-05-27 13:33:15 +0300

    Updated code

--- my_library.h
+++ my_library.h
@@ -1,58 +1,29 @@
-#ifndef MY_LIBRARY_H // Patikrina, ar MY_LIBRARY_H dar nėra apibrėžtas
-#define MY_LIBRARY_H // Jei nėra apibrėžtas, apibrėžia MY_LIBRARY_H (kad kitą kartą būtų atpažintas)
+#ifndef MY_LIBRARY_H
+#define MY_LIBRARY_H
 
 #include <iostream>
 #include <vector>
-#include <string>
-#include <iomanip>
-#include <numeric>  
-#include <algorithm> 
-#include <fstream>
-#include <sstream>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
 #include <chrono>
-#include <stdexcept>
 #include <random>
-#include <utility>
-#include <list>
-#include <deque>
-#include <cassert>
+#include <fstream>
+#include <map>
+#include <cctype>
 
 using std::cout;
 using std::endl;
 using std::vector;
-using std::string;
-using std::cin;
-using std::ifstream;
-using std::ofstream;
-using std::setw;
-using std::fixed;
-using std::setprecision;
-using std::left;
-using std::sort;
-using std::accumulate;
-using std::istringstream;
-using std::random_device;
-using std::mt19937;
-using std::uniform_int_distribution;
-using std::to_string;
-using std::pair;
-using std::list;
-using std::deque;
+using std::set;
+using std::unordered_set;
 using std::find;
-using std::find_if;
-using std::search;
-using std::copy;
-using std::remove;
-using std::remove_if;
-using std::remove_copy;
-using std::remove_copy_if;
-using std::transform;
-using std::partition;
-using std::stable_partition;
-using std::move;
-using std::swap;
-using std::exchange;
-using std::stringstream;
+using std::mt19937;
+using std::random_device;
+using std::uniform_int_distribution;
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::microseconds;
+using std::chrono::milliseconds;
 
-
-#endif  // Baigiame header guard (apsauga nuo pakartotinio įtraukimo)
+#endif // MY_LIBRARY_H
